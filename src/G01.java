3942c458081ae42720bedbfdd952cf49a1e80a58
sametyigit <samettyigitt0@gmail.com>
2023-04-01 05:06:53 +0300

    ilkgithubv2

--- src/G01.java
+++ src/G01.java
@@ -1,5 +1,7 @@
 public class G01 {
     public static void main(String[] args) {
         System.out.println("github ogreniyorum");
+        System.out.println("======================");
+        System.out.println("have a wise day");
     }
 }
